184c7661eaa30001a00bef5f5c8c36a9ee3482bf
Lenar N <nurmievl@outlook.com>
2022-11-13 19:54:32 -0400

    deleting system updates

--- application.py
+++ application.py
@@ -1,8 +1,5 @@
 print("TEST message?");
 print("message for new-api branch");
 print("; added by second developer!");
-<<<<<<< HEAD
 print("first developer comment")
-=======
 print("second developer comment");
->>>>>>> 4d6a0ac4eea55af27ace49546a85c0a3b4bfcf16
