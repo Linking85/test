2f4c92b38ebb006959bdb9e806d73e5b2453bca8
NurmievL <nurmievl@outlook.com>
2022-11-14 17:04:32 +0300

    1. application.py issues fixed - deleted ";" 2. new branch added - develop.

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
-print("TEST message?");
-print("message for new-api branch");
-print("; added by second developer!");
+print("TEST message?")
+print("message for new-api branch")
+print("; added by second developer!")
 print("first developer comment")
-print("second developer comment");
+print("second developer comment")
